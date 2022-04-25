d7d27537cf0cc00d2a1940c33cb8e44ffc62923a
Scriper.irk <scriper.irk@gmail.com>
2022-04-26 00:08:14 +0800

    Step_6

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(750);
         int expect = 250;
 
-        assertEquals(actual, expect);
+        assertEquals(expect, actual);
     }
 
     @Test
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1000);
         int expect = 0;
 
-        assertEquals(actual, expect);
+        assertEquals(expect, actual);
     }
 
     @Test
@@ -28,7 +28,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(7700);
         int expect = 300;
 
-        assertEquals(actual, expect);
+        assertEquals(expect, actual);
     }
 
     @Test
@@ -36,7 +36,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
         int expect = 0;
 
-        assertEquals(actual, expect);
+        assertEquals(expect, actual);
     }
 
 }
